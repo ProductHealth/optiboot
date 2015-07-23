825435e8a6beeebe723addfb6e94243683133861
Ben Pirt <ben@pirt.co.uk>
2015-07-23 09:07:42 +0100

    Configure the LED pin

--- optiboot/bootloaders/optiboot/optiboot.c
+++ optiboot/bootloaders/optiboot/optiboot.c
@@ -564,6 +564,7 @@ void CheckFlashImage() {
 
     uint16_t b, i, nextAddress=0;
 
+    // Turn on the LED pin
     LED_PIN |= _BV(LED);
     for (i=0; i<imagesize; i+=2)
     {
@@ -587,6 +588,7 @@ void CheckFlashImage() {
         nextAddress += SPM_PAGESIZE;
       }
     }
+    // Turn off the LED pin
     LED_PIN &= ~_BV(LED);
 
 #if defined(RWWSRE)
